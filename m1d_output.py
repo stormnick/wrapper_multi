30d858127dbbabc5ecc2a672370080016ebf8b26
Ekaterina Semenova <semenova@mpia.de>
2021-02-25 17:08:37 +0100

    testing on the cluster

--- m1d_output.py
+++ m1d_output.py
@@ -146,8 +146,8 @@ class m1d(object):
         self.xnorm  = f.read_reals(np.float32)
         self.height = f.read_reals(np.float32)
 
-        print(read_str(f, dtype=None).split())
-        # self.atmosid, self.dpid, self.dptype = f77_string(f, dtype='a').split()
+        # print(read_str(f, dtype=None).split())
+        self.atmosid, self.dpid, self.dptype = read_str(f, dtype=None).split()
 
         self.vturb = f.read_reals(np.float32)
 
