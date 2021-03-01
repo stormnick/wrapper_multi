3458ad61832e4e0e505bd1f6529881e3df5102f5
Ekaterina Semenova <semenova@mpia.de>
2021-03-01 16:12:55 +0100

    intermid

--- parallel_worker.py
+++ parallel_worker.py
@@ -81,7 +81,7 @@ def setup_multi_job(setup, job):
     print('AA',job, job.output)
 
 
-    return job
+    return
 
 
 def run_multi( job, atom, atmos):
@@ -206,7 +206,7 @@ def collect_output(setup):
 
 
 def run_serial_job(setup, job):
-        job = setup_multi_job( setup, job )
+        setup_multi_job( setup, job )
         print("job # %5.0f: %5.0f M1D runs" %( job.id, len(job.atmos) ) )
         for i in range(len(job.atmos)):
             # model atom is only read once
@@ -215,3 +215,4 @@ def run_serial_job(setup, job):
             atmos = model_atmosphere(file = job.atmos[i], format = setup.atmos_format)
             run_multi( job, atom, atmos)
         # shutil.rmtree(job['tmp_wd'])
+        return
