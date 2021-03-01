21944ae06be82e487ef15d47353aeee5391f0b3a
Ekaterina Semenova <semenova@mpia.de>
2021-03-01 16:06:17 +0100

    intermid

--- run_multi.py
+++ run_multi.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from init_run import setup, job
+from init_run import setup, serial_job
 from parallel_worker import run_serial_job, collect_output
 import multiprocessing
 from multiprocessing import Pool
