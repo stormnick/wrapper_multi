32dd87ed0d04469456440143035befeff276ccf5
Ekaterina Semenova <semenova@mpia.de>
2021-03-02 13:53:08 +0100

    small addition ot the header

--- atom_package.py
+++ atom_package.py
@@ -185,6 +185,8 @@ class bbline():
         self.nq = int(data_line.split()[3])
         self.qmax, self.q0 = np.array(data_line.split()[4:6]).astype(float)
         self.iwide = int(data_line.split()[6])
+        print(data_line)
+        print(data_line.split()[7:-1])
         self.ga, self.gvw, self.gs = np.array(data_line.split()[7:-1]).astype(float)
         if len(data_line.split()) > 10:
             self.profile_type = data_line.split()[-1].strip()
