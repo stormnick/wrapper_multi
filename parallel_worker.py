2e9d1a6ee98f7e3ebac8935134e285d98aa9d9ea
Ekaterina Semenova <semenova@mpia.de>
2021-03-04 15:48:55 +0100

    debug

--- parallel_worker.py
+++ parallel_worker.py
@@ -154,7 +154,7 @@ def run_multi( job, atom, atmos):
             record_len = record_len + out.ndep * out.nk * 8
 
 
-            faux.write(" %s %10.4f %10.4f %10.4f %10.4f %10.2f %10.4f %10.0f \n" \
+            faux.write(" %s %10.4f %10.4f %10.4f %10.4f %10.2f %10.2f %10.4f %10.0f \n" \
                         %( atmos.id, atmos.teff, atmos.logg, atmos.feh,  atmos.alpha, atmos.mass, np.mean(atmos.vturb), out.abnd, record_len  ) )
 
             fbin.close()
