6cb2fa43447b5a63246501aca6548da7d0c793ca
Ekaterina Semenova <semenova@mpia.de>
2021-03-05 10:39:08 +0100

    test IDL1 file

--- parallel_worker.py
+++ parallel_worker.py
@@ -107,7 +107,7 @@ def run_multi( job, atom, atmos):
 
     """ Read M1D output if M1D run was successful """
     print('os.path.isfile:', os.path.isfile('./IDL1'))
-    print('os.path.isfile:', os.path.exists('./IDL1'))
+    print('os.path.exists:', os.path.exists('./IDL1'))
     if os.path.isfile('./IDL1'):
         out = m1d('./IDL1')
 
