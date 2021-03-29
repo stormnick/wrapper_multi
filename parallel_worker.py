9698b1681ed7c2d49b5c867bb7c1b596d080893c
Ekaterina Semenova <semenova@mpia.de>
2021-03-29 11:31:43 +0200

    test

--- parallel_worker.py
+++ parallel_worker.py
@@ -6,6 +6,9 @@ import numpy as np
 from atom_package import model_atom, write_atom
 from atmos_package import model_atmosphere, write_atmos_m1d, write_dscale_m1d
 from m1d_output import m1d, m1dline
+import multiprocessing
+import datetime
+
 
 
 def mkdir(s):
@@ -13,6 +16,33 @@ def mkdir(s):
         shutil.rmtree(s)
     os.mkdir(s)
     return
+
+def addRec_to_NLTEbin(binFile, atmosID, ndep, nk, tau, depart):
+    # writes a record into existing NLTE binary
+    # separate for each paralell job
+    # they will be combined later
+    fbin = open(binFile, 'ab')
+    record_len = 0
+
+    record_len = record_len + 500
+    fbin.write(str.encode('%500s' %atmosID))
+
+    record_len = record_len + 4
+    fbin.write(int(ndep).to_bytes(4, 'little'))
+
+    record_len = record_len + 4
+    fbin.write(int(nk).to_bytes(4, 'little'))
+
+    fbin.write(np.array(tau, dtype='f8').tobytes())
+    record_len = record_len + ndep * 8
+
+
+    fbin.write(depart.tobytes())
+    record_len = record_len + ndep * nk * 8
+
+    fbin.close()
+
+    return record_len
 
 def setup_multi_job(setup, job):
     """
@@ -28,6 +58,12 @@ def setup_multi_job(setup, job):
     (integer) k: an ID of an individual job within the run
     (object) setup: object of class setup, regulates a setup for the whole run
     """
+
+    """
+    Make sure that only one process at a time can access input files
+    """
+    lock = multiprocessing.Lock()
+    lock.acquire()
 
     """ Make a temporary directory """
     mkdir(job.tmp_wd)
@@ -76,9 +112,14 @@ def setup_multi_job(setup, job):
     elif job.output['write_ts'] == 0:
         pass
     else:
-        print("write_ts flag unrecognised, stoppped")
+        print("write_ts flag unrecognised, stopped")
         exit(1)
-    return
+    job.output.update({'save_idl1':setup.save_idl1})
+    if setup.save_idl1 == 1:
+        job.output.update({'idl1_folder' : setup.idl1_folder})
+
+    lock.release()
+    return job
 
 
 def run_multi( job, atom, atmos):
@@ -103,11 +144,9 @@ def run_multi( job, atom, atmos):
     """ Go to directory and run MULTI 1D """
     os.chdir(job.tmp_wd)
 
-    sp.call(['./multi1d.exe'] )
+    os.system('time ./multi1d.exe')
 
     """ Read M1D output if M1D run was successful """
-    print('os.path.isfile:', os.path.isfile('./IDL1'))
-    print('os.path.isfile:', os.path.exists('./IDL1'))
     if os.path.isfile('./IDL1'):
         out = m1d('./IDL1')
 
@@ -121,47 +160,27 @@ def run_multi( job, atom, atmos):
             with open(job.output['file_ew'], 'a')as f:
                 for kr in mask:
                     line = out.line[kr]
-                    f.write('%10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.6f %10.6f %10.4f\n' \
+                    f.write("%10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.6f %10.6f %10.4f # '%s'\n" \
                         %(atmos.teff, atmos.logg, atmos.feh, out.abnd, out.g[out.irad[kr]], out.ev[out.irad[kr]],\
-                            line.lam0, out.f[kr], out.weq[kr], out.weqlte[kr], np.mean(atmos.vturb)) )
+                            line.lam0, out.f[kr], out.weq[kr], out.weqlte[kr], np.mean(atmos.vturb), atmos.id ) )
 
         """ save  MULTI1D output in a common binary file in the format for TS """
         if job.output['write_ts'] == 1:
-
-            fbin = open(job.output['file_4ts'], 'ab')
             faux = open(job.output['file_4ts_aux'], 'a')
-
-            # length of the record in the binary file for this model atmosphere and abundance
-            record_len = 0
-
-            atmosID = str.encode('%500s' %atmos.id)
-            record_len = record_len + 500
-            fbin.write(atmosID)
-
-            ndep = int(out.ndep).to_bytes(4, 'little')
-            record_len = record_len + 4
-            fbin.write(ndep)
-
-            nk = int(out.nk).to_bytes(4, 'little')
-            record_len = record_len + 4
-            fbin.write(nk)
-
-            tau500 = np.array(out.tau, dtype='f8')
-            fbin.write(tau500.tobytes())
-            record_len = record_len + out.ndep * 8
-
+            # append record to binary grid file
             with np.errstate(divide='ignore'):
                 depart = np.array((out.n/out.nstar).reshape(out.ndep, out.nk), dtype='f8')
-            fbin.write(depart.tobytes())
-            record_len = record_len + out.ndep * out.nk * 8
-
+            record_len = addRec_to_NLTEbin(job.output['file_4ts'], atmos.id, out.ndep, out.nk, out.tau, depart)
 
             faux.write(" '%s' %10.4f %10.4f %10.4f %10.4f %10.2f %10.2f %10.4f %10.0f \n" \
                         %( atmos.id, atmos.teff, atmos.logg, atmos.feh,  atmos.alpha, atmos.mass, np.mean(atmos.vturb), out.abnd, record_len  ) )
-
-            fbin.close()
             faux.close()
-        os.remove('./IDL1')
+
+        if job.output['save_idl1'] == 0:
+            os.remove('./IDL1')
+        elif job.output['save_idl1'] == 1:
+            destin = job.output['idl1_folder'] + "/idl1.%s_%s_A(X)_%5.5f" %(atmos.id, atom.element.replace(' ',''), atom.abund)
+            shutil.move('./IDL1', destin)
     # no IDL1 file created after the run
     else:
         print("IDL1 file not found for %s A(X)=%.2f" %(atmos.id, atom.abund))
@@ -171,10 +190,10 @@ def run_multi( job, atom, atmos):
 
 
 def collect_output(setup, jobs):
-    from datetime import date
-    today = date.today().strftime("%b-%d-%Y")
+    today = datetime.date.today().strftime("%b-%d-%Y")
 
     """ Collect all EW grids into one """
+    datetime0 = datetime.datetime()
     print("Collecting grids of EWs")
     if setup.write_ew > 0:
         with open(setup.common_wd + '/output_EWgrid_%s.dat' %(today), 'w') as com_f:
@@ -188,14 +207,16 @@ def collect_output(setup, jobs):
     for line in data_all:
         if not line.startswith('#'):
             teff, logg, feh, abund = line.split()[0:4]
-            vmic = line.split()[-1]
+            vmic = line.split()[10]
+            atmosID = line.split()[-1]
             wave = line.split()[6]
             if not [wave, teff, logg, vmic, feh, abund]  in params:
                 params.append( [wave, teff, logg, vmic, feh, abund] )
             else:
-                print("WARNING: found repeating entrance at \n %s AA Teff=%s, log(g)=%s, Vturb=%s, [Fe/H]=%s, A(X)=%s " \
-                        %(wave, teff, logg, vmic, feh, abund) )
-
+                print("WARNING: found repeating entrance at \n %s AA Teff=%s, log(g)=%s, Vturb=%s, [Fe/H]=%s, A(X)=%s, atmos: %s " \
+                        %(wave, teff, logg, vmic, feh, abund, atmosID) )
+    datetime1 = datetime.datetime()
+    print(datetime.timedelta(datetime1-datetime0))
     """ #TODO sort the grids of EWs """
 
     """ Collect all TS formatted NLTE grids into one """
@@ -240,7 +261,7 @@ def run_serial_job(args):
         setup = args[0]
         job = args[1]
         print("job # %5.0f: %5.0f M1D runs" %( job.id, len(job.atmos) ) )
-        setup_multi_job( setup, job )
+        job = setup_multi_job( setup, job )
         for i in range(len(job.atmos)):
             # model atom is only read once
             atom = setup.atom
