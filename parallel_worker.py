8309f99db7f62db230cfd6840633c02416776c12
Ekaterina Semenova <semenova@mpia.de>
2021-04-28 15:49:21 +0200

    scale abundance to feh of model atmosphere

--- parallel_worker.py
+++ parallel_worker.py
@@ -270,9 +270,11 @@ def run_serial_job(args):
         for i in range(len(job.atmos)):
             # model atom is only read once
             atom = setup.atom
-            #scale abundance with [Fe/H] of the model atmosphere
-            atom.abund  =  job.abund[i] - job.atmos.feh
             atmos = model_atmosphere(file = job.atmos[i], format = setup.atmos_format)
+
+            #scale abundance with [Fe/H] of the model atmosphere
+            atom.abund  =  job.abund[i] - atmos.feh
+            
             run_multi( job, atom, atmos)
         # shutil.rmtree(job['tmp_wd'])
         return job
